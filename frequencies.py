3ca9287bdad15870705ce06b72737a04c0fa41d6
Krishna <krishna.prasanna_kumar@kcl.ac.uk>
2022-10-03 11:34:37 +0100

    second text

--- frequencies.py
+++ frequencies.py
@@ -4,12 +4,13 @@
 def frequencies(items):
     frequencies = {}
     for x in items:
-        if(frequencies[x]):
+        val = str(x)
+        if(frequencies[val]):
         
-            frequencies[x] = frequencies[x] + 1
+            frequencies[val] = frequencies[val] + 1
         
         else:
-            frequencies[x] = 0
+            frequencies[val] = 0
 
     # Your code goes here
     return frequencies
