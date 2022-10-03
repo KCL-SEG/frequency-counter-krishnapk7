7f17ad3e6665cefb49a20709389b00874e932444
Krishna <krishna.prasanna_kumar@kcl.ac.uk>
2022-10-03 11:25:45 +0100

    first test

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
+    for x in items:
+        if(x in frequencies.keys()):
+        
+            frequencies[x] = frequencies[x] + 1
+        
+        else:
+            frequencies[x] = 0
+
     # Your code goes here
     return frequencies
